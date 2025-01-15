a4d16a33432f7e8ed6663b8aec558be94e4e86b1
Abronz Vizcarra <vabronz@gmail.com>
2025-01-14 21:50:36 -0500

    added MatAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @Component({
